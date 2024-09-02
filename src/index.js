048361f607c381fbd05eb5f868f0618de9276a66
megaxd274 <kazemirova.l@gmail.com>
2024-09-02 11:33:57 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    const str = String(n);
+    const result = [];
+    for (const digit of str) {
+        result.unshift(digit);
+    }
+    return Number(result.join(""));
+};
